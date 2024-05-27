e6bbb518282761c69ad57815e9c039b03bd32680
zakilagi88 <zakiramadhanii88@gmail.com>
2024-05-27 17:06:47 +0700

    update keluarga -tambah

--- app/Filament/Clusters/HalamanKependudukan/Resources/KartuKeluargaResource/Pages/CreateKartukeluarga.php
+++ app/Filament/Clusters/HalamanKependudukan/Resources/KartuKeluargaResource/Pages/CreateKartukeluarga.php
@@ -132,7 +132,7 @@ class CreateKartukeluarga extends CreateRecord
                                                         ->map(function ($item) {
                                                             return [
                                                                 'key' => $item->kk_id,
-                                                                'value' => $item->kepalaKeluarga->nama_lengkap . ' - ' . $item->kk_id . ' - ' . $item->wilayah->wilayah_nama,
+                                                                'value' => $item->kepalaKeluarga?->nama_lengkap . ' - ' . $item->kk_id . ' - ' . $item->wilayah?->wilayah_nama,
                                                             ];
                                                         })->pluck('value', 'key')
 
